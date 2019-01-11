10a7c2c705f3eeec655638a06651ba36378283d4
Dat Nguyen <dat.nguyen@gbst.com>
2019-01-11 15:58:36 +0700

    'm'

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <nav>
+    <!--<nav>
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
-    </nav>
+    </nav> !-->
     <router-view/>
   </div>
 </template>
 
 <script>
+export default {
+  name: 'App'
+}
 </script>
 
 <style lang="scss">
